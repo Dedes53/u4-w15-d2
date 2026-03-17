6da9c1892d1123adebacd1e3c1cdb48ca2c542a4
Dedes <federico.lepore73@gmail.com>
2026-03-17 15:49:09 +0100

    esercizio 1

---
+++ src/es1/Main.java
@@ -0,0 +1,52 @@
+package es1;
+
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Set<String> distinte = new HashSet<>(); //parole distinte
+        Set<String> doppie = new HashSet<>(); //parole doppie
+
+        try {
+            System.out.println("Indica il numero di parole che desideri inserire");
+            int n = scan.nextInt();
+            scan.nextLine(); //buona prassi dopo nextInt()
+
+            if (n <= 0) {
+                System.out.println("Dai eh, il numero deve essere maggiore di 0, altrimenti cosa ci stiamo a fare qui!");
+                return;
+            }
+
+            for (int i = 1; i <= n; i++) {
+                System.out.println("Inserisci la parola n." + i + ": ");
+                String parola = scan.nextLine().trim();
+
+                if (parola.isEmpty()) {
+                    System.out.println("Dai, inserisci una parola valida almeno");
+                    i--;
+                    continue;
+                }
+
+                boolean aggiunta = distinte.add(parola); //add() ritorna falso se parola è già presente nella lista
+
+                if (!aggiunta) doppie.add(parola); //quindi la aggiungo alla lista delle doppie
+            }
+
+            System.out.println("\nParole doppie: " + doppie); //stampa parole doppie
+            System.out.println("Numero parole distinte: " + distinte.size()); //stampa numero degli elementi in distinte
+            System.out.println("Elenco delle parole distinte: " + distinte);
+        } catch (InputMismatchException e) {
+            System.out.println("Ma secondo te, potrai aggiungere una parola non intera?!");
+        } finally {
+            scan.close();
+        }
+
+
+    }
+}
